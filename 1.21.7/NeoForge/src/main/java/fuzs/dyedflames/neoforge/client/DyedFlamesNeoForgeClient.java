230d397ceb57db16a6bf8d1d3998ce4c69aea42c
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-03 16:40:01 +0200

    full 1.21.7 port

--- 1.21.7/NeoForge/src/main/java/fuzs/dyedflames/neoforge/client/DyedFlamesNeoForgeClient.java
+++ 1.21.7/NeoForge/src/main/java/fuzs/dyedflames/neoforge/client/DyedFlamesNeoForgeClient.java
@@ -2,7 +2,7 @@ package fuzs.dyedflames.neoforge.client;
 
 import fuzs.dyedflames.DyedFlames;
 import fuzs.dyedflames.client.DyedFlamesClient;
-import fuzs.dyedflames.neoforge.data.client.ModParticleProvider;
+import fuzs.dyedflames.data.client.ModParticleProvider;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
 import net.neoforged.api.distmarker.Dist;
